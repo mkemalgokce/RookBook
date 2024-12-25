679f41aa1337ef2fc57790e85dc5927e71fb99d8
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-12-25 21:55:24 +0300

    Create 'RookBookIOSApp'

---
+++ RookBookIOSApp/RookBookIOSApp/App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+// Copyright © 2024 Mustafa Kemal Gökçe. All rights reserved.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // MARK: - Properties
+    var window: UIWindow?
+
+    // MARK: - Internal Methods
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
