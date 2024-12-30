529163d3aea4746570c889c1a865cc66172c28db
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-12-30 12:50:11 +0300

    Create new test targets 'CI-iOS' and 'CI-macOS'

---
+++ RookBook/RookBookCoreTestHostApp/ContentView.swift
@@ -0,0 +1,9 @@
+// Copyright © 2024 Mustafa Kemal Gökçe. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {}
+    }
+}
---
+++ RookBook/RookBookCoreTestHostApp/RookBookCoreTestHostAppApp.swift
@@ -0,0 +1,13 @@
+// Copyright © 2024 Mustafa Kemal Gökçe. All rights reserved.
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+@main
+struct RookBookCoreTestHostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
